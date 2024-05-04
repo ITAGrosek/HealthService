c78b1038a2a76e15697b10382cea89515a6a944c
martingrosek <martin.grosek@student.um.si>
2024-05-04 10:54:01 +0200

    dela

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Dockerfile
+FROM node:16-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 4000
+
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Health Check Service is running!');
+});
+
+async function checkHttpService(url, name) {
+    try {
+        const response = await axios.get(url);
+        return { name, status: 'Healthy', statusCode: response.status };
+    } catch (error) {
+        return { name, status: 'Unhealthy', error: error.message };
+    }
+}
+
+app.get('/health', async (req, res) => {
+    try {
+        // For local development
+        // const bookServiceHealth = await checkHttpService('http://localhost:8080', 'Book Management Service');
+        // const reservationServiceHealth = await checkHttpService('http://localhost:8081/reservations', 'Reservation Service');
+        // const userServiceHealth = await checkHttpService('http://localhost:3000/api/users', 'User Service');
+
+        // For Docker
+        const bookServiceHealth = await checkHttpService('http://book-management-service:8080/api', 'Book Management Service');
+        const reservationServiceHealth = await checkHttpService('http://reservation-service:8081', 'Reservation Service');
+        const userServiceHealth = await checkHttpService('http://bff-web:3000/api/users', 'User Service');
+
+        res.json([bookServiceHealth, reservationServiceHealth, userServiceHealth]);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to perform health check' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
